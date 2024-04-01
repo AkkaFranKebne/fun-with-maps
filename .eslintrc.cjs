3d3d4b21aafcea9b6530c5b44fb3c3b20b6b6743
Ewa Wrobel <ewa.wrobel@tink.com>
2024-04-01 20:38:12 +0200

    update lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,54 @@
+const { defineConfig } = require("eslint-define-config");
+
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
-}
-
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  overrides: [
+    {
+      files: ["src/**/*.{ts,tsx}"],
+      plugins: ["@typescript-eslint"],
+      parser: "@typescript-eslint/parser",
+      rules: {
+        "@typescript-eslint/no-unused-vars": 0,
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+      },
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+      },
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+        "prettier",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:react/jsx-runtime",
+      ],
+    },
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      env: {
+        node: true,
+        browser: true,
+        es2021: true,
+      },
+      extends: ["eslint:recommended"],
+      rules: {
+        "no-unused-vars": 0,
+      },
+    },
+  ],
+};
